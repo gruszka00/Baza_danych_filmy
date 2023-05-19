3cf1c6b99673df433c3c3b5423c309a9a7bb13ba
gerardgruszka <gerard.gruszka00@gmail.com>
2023-05-19 17:00:15 +0200

    update

--- main.py
+++ main.py
@@ -8,11 +8,53 @@ mydb = mysql.connector.connect(
   database='Films'
 )
 
-query= 'SELECT title, year from Films'
-
 cursor = mydb.cursor()
-cursor.execute(query)
+cursor.execute("USE Films")
+cursor.execute("ALTER TABLE Films AUTO_INCREMENT=1")
 
-for films in cursor:
-  print(films)
 
+
+
+
+class Film:
+  def __init__(self, id, title, year):
+    self.id = id
+    self.title = title
+    self.year = year
+
+  def save(self):
+    query = "SELECT * FROM Films WHERE id = " + str(self.id)
+    cursor.execute(query)
+    row = cursor.fetchone()
+    if row is None:
+     query = "INSERT INTO Films (title, year) SELECT " \
+     "'" + self.title + "', " \
+     "'" + self.year + "', "
+     cursor.execute(query)
+    else:
+      query = "UPDATE Films SET " \
+      "id = " + str(self.id) + ", " \
+      "title = '" + self.title + "', " \
+      "year = '" + self.year + "', " \
+      " WHERE id = " + str(self.id)
+      cursor.execute(query)
+
+  @staticmethod
+  def all():
+    list = []
+    cursor.execute("SELECT * FROM Films")
+    for A in cursor:
+      list.append(Film(A[0], A[1], A[2]))
+    return list
+
+def addc(list, title, year):
+  list.append(Film(list[-1].id + 1, title, year))
+
+filmy = Film.all()
+addc(filmy,"Prestige","2010")
+filmy[-1].save()
+print(filmy)
+
+mydb.commit()
+cursor.close()
+mydb.close()
