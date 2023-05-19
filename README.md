91846990f7c13fd849d0c400822a82e352d77279
gerardgruszka <gerard.gruszka00@gmail.com>
2023-05-19 17:42:23 +0200

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+Jest to program organizujący stworzoną przeze mnie bazę danych
+dotyczącą filmów. Tabel zawiera takie informacje jak id, tytuł filmu
+i jego rok produkcji. Możemy modyfikować tabalę, dodając nowe filmy.
+
--- main.py
+++ main.py
@@ -9,12 +9,6 @@ mydb = mysql.connector.connect(
 )
 
 cursor = mydb.cursor()
-cursor.execute("USE Films")
-cursor.execute("ALTER TABLE Films AUTO_INCREMENT=1")
-
-
-
-
 
 class Film:
   def __init__(self, id, title, year):
@@ -22,24 +16,27 @@ class Film:
     self.title = title
     self.year = year
 
+  def __str__ (self):
+    return f"Filmy: id: {self.id} Title: {self.title} Year:{self.year}"
+
   def save(self):
-    query = "SELECT * FROM Films WHERE id = " + str(self.id)
+    query = "SELECT * FROM Films WHERE id_film = " + str(self.id)
     cursor.execute(query)
     row = cursor.fetchone()
     if row is None:
-     query = "INSERT INTO Films (title, year) SELECT " \
+     query = "INSERT INTO Films(title, year) VALUE( " \
      "'" + self.title + "', " \
-     "'" + self.year + "', "
+     "'" + self.year + "') "
      cursor.execute(query)
     else:
       query = "UPDATE Films SET " \
-      "id = " + str(self.id) + ", " \
+      "id_film = " + str(self.id) + ", " \
       "title = '" + self.title + "', " \
       "year = '" + self.year + "', " \
-      " WHERE id = " + str(self.id)
+      " WHERE id_film = " + str(self.id)
       cursor.execute(query)
 
-  @staticmethod
+
   def all():
     list = []
     cursor.execute("SELECT * FROM Films")
@@ -47,13 +44,17 @@ class Film:
       list.append(Film(A[0], A[1], A[2]))
     return list
 
-def addc(list, title, year):
+def add(list, title, year):
   list.append(Film(list[-1].id + 1, title, year))
 
+
+
+
 filmy = Film.all()
-addc(filmy,"Prestige","2010")
+
+add(filmy,"Prestige","2010")
 filmy[-1].save()
-print(filmy)
+
 
 mydb.commit()
 cursor.close()
